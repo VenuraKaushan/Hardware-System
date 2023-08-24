b1e0b2ebb662ccceecbf7f28fcdd40be0d4dbf38
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2023-08-25 03:31:10 +0530

    initialized folder structure and create both front and back ends

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import  express  from "express";
+import 'dotenv/config';
+import cors from 'cors';
+import dbConnect from "./configs/dbconfig";
+
+const app = express();
+
+const PORT = process.env.PORT || 6001;
+
+//cors initialization
+app.use(cors())
+
+//initialize the json
+app.use(express.json());
+
+// config the urlEncoded middleware
+app.use(express.urlencoded({extended : false}));
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} =====> URL: ${req.url}`);
+    next();
+});
+
+//config routes
+app.get('/',(req,res)=>{
+    res.send("Hardware PROJECT");
+});
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is started on port ${PORT}🚀👍`);
+    dbConnect();
+});
